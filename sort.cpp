16be30298e8fd77c00ab1c3310b78a85dad5189a
Ab03beed <abdaher2000@gmail.com>
2024-01-27 17:07:58 +0100

    Added sort class.

---
+++ sort.cpp
@@ -0,0 +1,11 @@
+//
+// Created by abdah on 2024-01-27.
+//
+
+#include "sort.h"
+
+
+void sort::selection(size_t size) {
+
+
+}
---
+++ sort.h
@@ -0,0 +1,22 @@
+//
+// Created by abdah on 2024-01-27.
+//
+
+#include <iostream>
+#include <vector>
+
+
+#ifndef DT046G_L2_SORT_H
+#define DT046G_L2_SORT_H
+
+
+class sort {
+public:
+    void selection(size_t size);
+private:
+    std::vector<int> generate;
+
+};
+
+
+#endif //DT046G_L2_SORT_H
